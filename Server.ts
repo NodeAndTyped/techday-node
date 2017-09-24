496a1bfbd22aa258be7a943b56f4fe38c21b67fb
romain.lenzotti <rom.lenzotti@gmail.com>
2017-09-24 11:52:47 +0200

    feature: ajout des middlewares

--- Server.ts
+++ Server.ts
@@ -1,9 +1,20 @@
-import {ServerLoader, ServerSettings} from "ts-express-decorators";
+import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "ts-express-decorators";
 
 @ServerSettings({
     acceptMimes: ["application/json"],
     debug: process.env.NODE_ENV !== "production"
 })
 export class Server extends ServerLoader {
+    $onMountingMiddlewares() {
+        const bodyParser = require('body-parser'),
+            methodOverride = require('method-override');
+        this
+            .use(GlobalAcceptMimesMiddleware)
+            .use(methodOverride())
+            .use(bodyParser.json())
+            .use(bodyParser.urlencoded({
+                extended: true
+            }));
 
+    }
 }
